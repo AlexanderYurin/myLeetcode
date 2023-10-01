b3965429a7fa493dc5f03ab7f4eb565947ebfefb
Alexander Yurin <hiimmandarin32@gmail.com>
2023-10-01 13:05:07 +0300

    new solution

--- String/3.py
+++ String/3.py
@@ -1,17 +1,21 @@
 def lengthOfLongestSubstring(s: str) -> int:
-	lenght_s = len(s)
+	length_s = len(s)
+	if length_s < 1:
+		return 0
 	result = s[0]
-	for i in range(1, lenght_s):
+	for i in range(length_s):
 		new_s = s[i]
-		for j in range(i + 1, lenght_s):
+		for j in range(i + 1, length_s):
 			if s[j] in new_s:
 				break
-			new_s += s[j]
-			if len(result) < len(s[i:j + 1]):
-				result = new_s
-
+			else:
+				new_s += s[j]
+		if len(result) < len(new_s):
+			result = new_s
 	return len(result)
 
 
-assert lengthOfLongestSubstring("bbbbbb") == 1
+assert lengthOfLongestSubstring("") == 0
+assert lengthOfLongestSubstring(" ") == 1
+assert lengthOfLongestSubstring("au") == 2
 assert lengthOfLongestSubstring("abbabc") == 3

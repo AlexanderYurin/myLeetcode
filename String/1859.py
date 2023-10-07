b548e7d1b3583d00980f2a163337dcf2444ba8d4
Alexander Yurin <hiimmandarin32@gmail.com>
2023-10-07 20:31:42 +0300

    1859 complete

---
+++ String/1859.py
@@ -0,0 +1,6 @@
+def sortSentence(s: str) -> str:
+	sort_text = list(map(lambda x: x[:len(x)-1], sorted(s.split(), key=lambda x: x[-1])))
+	return " ".join(sort_text)
+
+
+assert sortSentence(s="is2 sentence4 This1 a3") == "This is a sentence"

ce1b60af5fd4a2c4a281302762a10c8d802d0f17
Alexander Yurin <hiimmandarin32@gmail.com>
2023-10-20 15:58:50 +0300

    1684 review

--- String/1684.py
+++ String/1684.py
@@ -2,9 +2,16 @@ from typing import List
 
 
 def countConsistentStrings(allowed: str, words: List[str]) -> int:
-	return len([i for i in words if allowed in i])
+	count = 0
+	for word in words:
+		for letter in word:
+			if letter not in allowed:
+				break
+		else:
+			count += 1
+	return count
 
 
 if __name__ == '__main__':
 	assert countConsistentStrings(allowed="ab", words=["ad", "bd", "aaab", "baa", "badab"]) == 2
-	assert countConsistentStrings(allowed="abc", words=["a", "b", "c", "ab", "ac", "bc", "abc"]) == 1
+	assert countConsistentStrings(allowed="abc", words=["a", "b", "c", "ab", "ac", "bc", "abc"]) == 7

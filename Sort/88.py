bfc0f8c4dc3152b7b2fb477f75e48f338098705c
Alexander Yurin <hiimmandarin32@gmail.com>
2023-10-29 19:31:47 +0300

    88 complete

---
+++ Sort/88.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+	"""
+    Do not return anything, modify nums1 in-place instead.
+    """
+	for i in nums2:
+		nums1.append(i)
+		nums1.remove(0)
+	nums1.sort()
+
+
+
+
+if __name__ == '__main__':
+	nums1 = [1, 2, 3, 0, 0, 0]
+	assert merge(nums1=nums1, m=3, nums2=[2, 5, 6], n=3) is None
+	print(nums1)

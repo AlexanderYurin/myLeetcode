355e6a4d640b59f6ffc4047ec75071f98b799e81
Alexander Yurin <hiimmandarin32@gmail.com>
2023-12-09 22:22:01 +0300

    283 complete

--- Array/283.py
+++ Array/283.py
@@ -1,19 +1,28 @@
-from typing import List
+# from typing import List
+#
+#
+# def moveZeroes(nums: List[int]) -> None:
+# 	"""
+#     Do not return anything, modify nums in-place instead.
+#     """
+#
+# 	# nums.sort(key=lambda x: x == 0)
+# 	# return nums
+# 	n = 0
+# 	for i in range(len(nums)):
+# 		if nums[i]:
+# 			nums[n], nums[i] = nums[i], nums[n]
+# 			n += 1
+#
+#
+# if __name__ == '__main__':
+# 	assert moveZeroes([0, 0, 1]) == [1, 0, 0]
+from collections import Counter
 
 
-def moveZeroes(nums: List[int]) -> None:
-	"""
-    Do not return anything, modify nums in-place instead.
-    """
-
-	# nums.sort(key=lambda x: x == 0)
-	# return nums
-	n = 0
-	for i in range(len(nums)):
-		if nums[i]:
-			nums[n], nums[i] = nums[i], nums[n]
-			n += 1
+def isSubsequence(s: str, t: str) -> bool:
+    print(list(Counter("".join([let for let in t if let in s])).keys()) == list(Counter(s).keys()))
+    return s in "".join([let for let in t if let in s])
 
 
-if __name__ == '__main__':
-	assert moveZeroes([0, 0, 1]) == [1, 0, 0]
+isSubsequence("abc", "ahbgdc")

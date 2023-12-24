195d09949f97394b34b73cacde674744edd3673e
Alexander Yurin <hiimmandarin32@gmail.com>
2023-12-24 10:14:18 +0300

    605 complete

--- Array/605.py
+++ Array/605.py
@@ -2,28 +2,29 @@ from typing import List
 
 
 def canPlaceFlowers(flowerbed: List[int], n: int) -> bool:
-	left = None
+	left = 0
 	mid = 0
-	right = flowerbed[1]
+	right = 1
 	count = 0
 
 	while mid < len(flowerbed):
-		if left is None and flowerbed[mid] == 0 and right == 0:
+		if right >= len(flowerbed):
+			right = mid
+		if flowerbed[left] == 0 and \
+			flowerbed[mid] == 0 and \
+			flowerbed[right] == 0:
+			flowerbed[mid] = 1
 			count += 1
-			left = flowerbed[right]
-			mid = right + 1
-			right = mid + 1
-			if right >= len(flowerbed):
-				right = None
+		left = mid
+		mid = left + 1
+		right = mid + 1
 
-
-
-
+	return count >= n
 
 
 if __name__ == '__main__':
-	assert canPlaceFlowers(flowerbed=[0, 0, 1, 0, 1], n=1) is True
+	assert canPlaceFlowers(flowerbed=[0, 1, 0, 1, 0, 1, 0, 0], n=1) is True
 	assert canPlaceFlowers(flowerbed=[0, 1, 0], n=1) is False
 	assert canPlaceFlowers(flowerbed=[1, 0, 1], n=1) is False
 	assert canPlaceFlowers(flowerbed=[1, 0, 0], n=1) is True
-	assert canPlaceFlowers(flowerbed=[1, 0, 0, 1, 0], n=2) is True
+	assert canPlaceFlowers(flowerbed=[1, 0, 0, 1, 0], n=2) is False

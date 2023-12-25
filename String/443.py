d4b9203eed79b7f622009cef8af00a08960ebc29
Alexander Yurin <hiimmandarin32@gmail.com>
2023-12-25 16:46:54 +0300

    443 complete

--- String/443.py
+++ String/443.py
@@ -2,28 +2,26 @@ from typing import List
 
 
 def compress(chars: List[str]) -> int:
-	i, j, c = 0, 1, 1
-	while j < len(chars):
-		if chars[i] == chars[j]:
+	chars.append(" ")
+	i, c = 1, 1
+	while i < len(chars):
+		if chars[i] == chars[i - 1]:
+			chars.pop(i)
 			c += 1
-			chars.pop(j)
 		else:
-			if c > 1:
-				chars.insert(j, *list(str(c)))
-				c = 1
-				i = j + 1
-				j += 2
+			if c == 1:
+				i += 1
 			else:
-				i = j
-				j += 1
-	if c > 1:
-		chars.insert(j, *list(str(c)))
+				for j in str(c):
+					chars.insert(i, j)
+					i += 1
+				i += 1
+				c = 1
+	chars.pop()
 	return len(chars)
 
 
-
 if __name__ == '__main__':
-	char = ["a", "a", "b", "b", "c", "c", "c"]
-	assert compress(char) == 6
-	print(char)
-	assert compress(chars=["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b"]) == 5
+	# char = ["a", "a", "b", "b", "c", "c", "c"]
+	# assert compress(char) == 6
+	assert compress(chars=["a", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "b", "c"]) == 5

0b0f8a5c2491023a5cef57cab1ba0528761d80b3
Alexander Yurin <hiimmandarin32@gmail.com>
2023-12-30 10:27:06 +0300

    2352 complete

--- HashTable/2352.py
+++ HashTable/2352.py
@@ -1,12 +1,16 @@
 from typing import List
+from collections import Counter
 
 
 def equalPairs(grid: List[List[int]]) -> int:
-    columns = [[row[i] for row in grid] for i in range(len(grid))]
-    return 1a
-
-
+	rows = Counter(map(tuple, grid))
+	columns = ((row[i] for row in grid) for i in range(len(grid)))
+	count = 0
+	for col in columns:
+		count += rows[tuple(col)]
+	return count
 
 
 if __name__ == '__main__':
-    assert equalPairs(grid=[[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]]) == 3
+	assert equalPairs(grid=[[13, 13], [13, 13]]) == 4
+	assert equalPairs(grid=[[3, 1, 2, 2], [1, 4, 4, 5], [2, 4, 2, 2], [2, 4, 2, 2]]) == 3

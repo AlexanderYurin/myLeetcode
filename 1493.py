edfcc983769085d7889c3f2987bf99809cff22e5
Alexander Yurin <hiimmandarin32@gmail.com>
2024-01-10 17:57:27 +0300

    1493 complete

--- 1493.py
+++ 1493.py
@@ -2,20 +2,23 @@ from typing import List
 
 
 def longestSubarray(nums: List[int]) -> int:
-    i = j = 0
-    count = 0
-    max_curr = 0
-    for j in range(len(nums)):
-        if nums[j] == 0 and count == 0:
-            i = j
-            count += 1
-        if count == 1:
-            i += 1
-        max_curr = max(j-i, max_curr)
+	i = k = 0
+	count = 0
+	max_curr = 0
+	for j in range(len(nums)):
+		if nums[j] == 0:
+			if count == 1:
+				i, k = k + 1, j
+			else:
+				k = j
+				count = 1
+		max_curr = max(j - i, max_curr)
 
-    return max_curr
+	return max_curr
 
 
 if __name__ == '__main__':
-    assert longestSubarray(nums=[1, 1, 0, 1]) == 3
-    assert longestSubarray(nums=[0, 1, 1, 1, 0, 1, 1, 0, 1]) == 5
+	assert longestSubarray(nums=[1, 1, 1]) == 2
+	assert longestSubarray(nums=[0, 0, 0, 0]) == 0
+	assert longestSubarray(nums=[1, 1, 0, 1]) == 3
+	assert longestSubarray(nums=[0, 1, 1, 1, 0, 1, 1, 0, 1]) == 5

c9fcc584010958ff4f907db3d2397f5065f95329
Alexander Yurin <hiimmandarin32@gmail.com>
2024-01-31 22:41:11 +0300

    complete

---
+++ sort_zero.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+
+def sort_zero(array):
+	result = deque()
+	for i in range(len(array)-1, -1, -1):
+		if array[i] == 0:
+			result.append(array[i])
+		else:
+			result.appendleft(array[i])
+	return result
+
+
+if __name__ == '__main__':
+	assert sort_zero([0, 0, 1, 2, 3]) == [1, 2, 3, 0, 0]

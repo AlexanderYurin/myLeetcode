c95e6f98b30226ea61ec266952efea7e72439a99
Alexander Yurin <hiimmandarin32@gmail.com>
2024-02-18 17:46:09 +0300

    735 complete

--- 735.py
+++ 735.py
@@ -2,41 +2,40 @@ from typing import List
 
 
 def asteroidCollision(asteroids: List[int]) -> List[int]:
-    stack = []
-    for asteroid in asteroids:
-        if len(stack) == 0:
-            stack.append(asteroid)
-            continue
+	route_asteroids = []
+	for i in range(len(asteroids)):
+		if len(route_asteroids) == 0:
+			route_asteroids.append(asteroids[i])
+			continue
+		left_asteroid = route_asteroids.pop()
+		asteroid = asteroids[i]
+		if left_asteroid > 0 > asteroid:
+			while left_asteroid and asteroid:
+				if abs(left_asteroid) > abs(asteroid):
+					asteroid = None
+				elif abs(left_asteroid) < abs(asteroid):
+					if len(route_asteroids) > 0:
+						left_asteroid = route_asteroids.pop()
+						if left_asteroid < 0 > asteroid or left_asteroid > 0 < asteroid:
+							route_asteroids += [left_asteroid, asteroid]
+							break
+					else:
+						left_asteroid = None
+				else:
+					left_asteroid = asteroid = None
+					break
 
-        last_asteroid = stack.pop()
-
-        if last_asteroid + asteroid == 0:
-            continue
-
-        if max(last_asteroid, asteroid) < last_asteroid + asteroid or min(last_asteroid,
-                                                                          asteroid) > last_asteroid + asteroid:
-            stack.append(last_asteroid)
-            stack.append(asteroid)
-            continue
-
-        while 1:
-            if last_asteroid < 0:
-                if abs(last_asteroid) < asteroid:
-                    stack.append(asteroid)
-                    break
-            else:
-                if abs(asteroid) < last_asteroid:
-                    stack.append(last_asteroid)
-                    break
-                else:
-                    stack.append(asteroid)
-                    break
-            
-    return stack
-
+			else:
+				route_asteroids.append(left_asteroid or asteroid)
+		else:
+			route_asteroids += [left_asteroid, asteroid]
+	return route_asteroids
 
 
 if __name__ == '__main__':
-    # assert asteroidCollision(asteroids=[5, 10, -5]) == [5, 10]
-    # assert asteroidCollision(asteroids=[5, -5]) == []
-    assert asteroidCollision(asteroids=[10, 4, -11]) == [-11]
+	assert asteroidCollision([1, -1, -2, -2]) == [-2, -2]
+	assert asteroidCollision([-2, -2, 1, -2]) == [-2, -2, -2]
+	assert asteroidCollision([-2, -1, 1, 2]) == [-2, -1, 1, 2]
+	assert asteroidCollision(asteroids=[5, 10, -5]) == [5, 10]
+	assert asteroidCollision(asteroids=[5, -5]) == []
+	assert asteroidCollision(asteroids=[10, 4, -11]) == [-11]

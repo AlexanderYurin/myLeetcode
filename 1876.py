bc50f0abdfe4040d544445902cdd5cfc0e288193
Alexander Yurin <hiimmandarin32@gmail.com>
2024-03-02 21:35:50 +0300

    1876 complete

---
+++ 1876.py
@@ -0,0 +1,7 @@
+def countGoodSubstrings(s: str) -> int:
+	return len([1 for i in range(0, len(s)) if len(set(s[i:i+3])) == 3])
+
+
+if __name__ == '__main__':
+	assert countGoodSubstrings(s = "xyzzaz") == 1
+	assert countGoodSubstrings(s = "aababcabc") == 4

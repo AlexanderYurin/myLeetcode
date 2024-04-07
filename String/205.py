23920903bfc84f90ae21589c25d0fdb203725099
Alexander Yurin <hiimmandarin32@gmail.com>
2024-04-07 16:48:45 +0300

    205 complete

--- String/205.py
+++ String/205.py
@@ -1,7 +1,11 @@
+from collections import Counter
+
+
 def isIsomorphic(s: str, t: str) -> bool:
-    if len(s) != len(t):
-        return False
+    return all(j[1] == i[1] for i, j, in zip(Counter(s).items(), Counter(t).items()))
 
 
 if __name__ == '__main__':
- pass
+    assert isIsomorphic("egg", "vpp") is True
+    assert isIsomorphic("bbbaaaba", "aaabbbba") is False
+    assert isIsomorphic("paper", "title") is True

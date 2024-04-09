bcc73361dd19e769f5c2b34c201a08f55354f277
Alexander Yurin <hiimmandarin32@gmail.com>
2024-04-09 19:18:15 +0300

    17 complete

--- HashTable/17.py
+++ HashTable/17.py
@@ -1,41 +1,38 @@
 from typing import List
 
+
 def letterCombinations(digits: str) -> List[str]:
-	from collections import deque
-	dict_letters = {
-		"2": ["a", "b", "c"],
-		"3": ["d", "e", "f"],
-		"4": ["g", "h", "i"],
-		"5": ["j", "k", "l"],
-		"6": ["m", "n", "o"],
-		"7": ["p", "q", "r", "s"],
-		"8": ["t", "u", "v"],
-		"9": ["w", "x", "y", "z"]
-	}
 	result = []
+	dict_letters = {
+		"2": "abc", '3': "def", '4': "ghi", '5': "jkl",
+		'6': "mno", '7': "pqrs", '8': "tuv", '9': "wxyz"
+	}
 
-	if len(digits) <= 1:
-		return dict_letters.get(digits[:1], [])
+	def get_letter_comb(nums, word=""):
+		if not nums:
+			return word
+		for let in dict_letters[nums[0]]:
+			word += let
+			word = get_letter_comb(nums[1:], word) or word
+			if len(word) == len(digits):
+				result.append(word)
+			word = word[:len(word)-1] or ""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	get_letter_comb(digits)
+	return result
 
 
 if __name__ == '__main__':
-	assert letterCombinations(digits="") == []
+	assert letterCombinations("999") == ["www", "wwx", "wwy", "wwz", "wxw", "wxx", "wxy", "wxz", "wyw", "wyx", "wyy",
+										 "wyz", "wzw", "wzx", "wzy", "wzz", "xww", "xwx", "xwy", "xwz", "xxw", "xxx",
+										 "xxy", "xxz", "xyw", "xyx", "xyy", "xyz", "xzw", "xzx", "xzy", "xzz", "yww",
+										 "ywx", "ywy", "ywz", "yxw", "yxx", "yxy", "yxz", "yyw", "yyx", "yyy", "yyz",
+										 "yzw", "yzx", "yzy", "yzz", "zww", "zwx", "zwy", "zwz", "zxw", "zxx", "zxy",
+										 "zxz", "zyw", "zyx", "zyy", "zyz", "zzw", "zzx", "zzy", "zzz"]
+	assert letterCombinations("234") == ["adg", "adh", "adi", "aeg", "aeh", "aei", "afg", "afh", "afi", "bdg", "bdh",
+										 "bdi", "beg", "beh", "bei",
+										 "bfg", "bfh", "bfi", "cdg", "cdh", "cdi", "ceg", "ceh", "cei", "cfg", "cfh",
+										 "cfi"]
+	assert letterCombinations(digits="22") == ["aa", "ab", "ac", "ba", "bb", "bc", "ca", "cb", "cc"]
+	assert letterCombinations(digits="23") == ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
 	assert letterCombinations(digits="2") == ["a", "b", "c"]
